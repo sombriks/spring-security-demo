0e8f85a212493a905bd1689fa2e316f98fa51b09
Leonardo Silveira <sombriks@gmail.com>
2025-11-15 20:39:38 -0300

    sampling jwt configuration

--- src/main/java/com/example/demo/AuthConfig.java
+++ src/main/java/com/example/demo/AuthConfig.java
@@ -2,11 +2,25 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AuthConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(MyLoginRepository repository) {
+        return username -> {
+            // create a custom call on your repository to return MyLogin by username
+            // Optional<MyLogin> getByLogin(String login);
+            return repository
+                    .getByLogin(username)
+                    .map(UserDetailsDTO::new)
+                    .orElseThrow(() -> new UsernameNotFoundException(username));
+        };
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -17,6 +18,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
         return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(withDefaults())
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
